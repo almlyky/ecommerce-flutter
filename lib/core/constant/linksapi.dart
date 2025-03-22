a029b67a46926bc10c00ed3aa170bacd73ed93d2
Abubaker <abubaker773880@gmail.com>
2025-03-23 00:55:06 +0300

    update linkapi

--- lib/core/constant/linksapi.dart
+++ lib/core/constant/linksapi.dart
@@ -1,40 +1,5 @@
-const String linkserver = "http://10.0.2.2:80/ecommerceApp";
-
-// const String linkserver="http://10.0.2.2/electronic store project";
-// const String linkssignup="$linkserver/signup.php";
-// const String linkssignup="https://jsonplaceholder.typicode.com/posts";
-// const String linkssignup="$linkserver/api/getdata.php";
-
-const String linkssignup =
-    "https://ecommerceapp773880.000webhostapp.com/ecommerceApp/index.php";
-const String linklogin =
-    "https://ecommerceapp773880.000webhostapp.com/ecommerceApp/login.php";
-const String linkproduct =
-    "https://ecommerceapp773880.000webhostapp.com/ecommerceApp/product.php";
-const String linkcat =
-    "https://ecommerceapp773880.000webhostapp.com/ecommerceApp/categories.php";
-const String linkpython = "http://192.168.1.4:8080";
-
-//link xampp
-const String linkcat2 = "$linkserver/categories.php";
-const String linkproduct2 = "$linkserver/product.php";
-const String image = "http://10.0.2.2:8000";
-const String linklogin2 = "$linkserver/login.php";
-const String linkssignup2 = "$linkserver/signup.php";
-const String linkinsertdata2 = "$linkserver/insertdata.php";
-const String linkdeletedata2 = "$linkserver/deletedata.php";
-const String linkedetedata2 = "$linkserver/edetedata.php";
-const String linkfavorite = "$linkserver/favorite.php";
-
-// int? catfk;
-// String djproductlist="http://10.0.2.2:8000/pages/product_cat/$catfk/";
-
 //=============================== link api django========================================
-// const djServer = "http://192.168.1.4:8000";
 // const djServer = "http://10.0.2.2:8000";
-// const djServer = "http://192.168.1.2:8000";
-// const djServer = "http://192.168.8.33:8000";
-// const djServer = "http://0.0.0.0:8000";
 const djServer = "https://abubaker77.pythonanywhere.com";
 
 // api categories
